aa264b808c4216121a5e14bdf14d50e0dfa92487
craigfowler <craig@craigfowler.me.uk>
2023-09-11 16:33:32 +0100

    Inner text not HTML

--- displayFormattedObject.ts
+++ displayFormattedObject.ts
@@ -6,9 +6,9 @@ export function displayFormattedObject(
   divElement: HTMLDivElement
 ) {
   const unmapped = ko.mapping.toJS(mappedObject);
-  const codePreview = divElement.querySelector('code pre');
+  const codePreview = <HTMLPreElement>divElement.querySelector('code pre');
   const valueElement = divElement.querySelector('p');
 
   valueElement.innerText = `The name is ${mappedObject.firstName()}, with ${mappedObject.numberOfPets()} pets`;
-  codePreview.innerHTML = JSON.stringify(unmapped, undefined, 4);
+  codePreview.innerText = JSON.stringify(unmapped, undefined, 4);
 }
